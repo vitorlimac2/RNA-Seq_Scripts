9bc2ebeb449f92c8c19e647232c1ca64aa6e2e01
vitor <99luftballons>
2018-05-18 11:53:38 -0300

    remove soft clipping from bam

---
+++ removeSoftClippingFromBAM.sh
@@ -0,0 +1,3 @@
+INPUT_BAM=$1
+
+samtools view $INPUT_BAM | awk 'BEGIN {OFS="\t"} {split($6,C,/[0-9]*/); split($6,L,/[SMDIN]/); if (C[2]=="S") {$10=substr($10,L[1]+1); $11=substr($11,L[1]+1)}; if (C[length(C)]=="S") {L1=length($10)-L[length(L)-1]; $10=substr($10,1,L1); $11=substr($11,1,L1); }; gsub(/[0-9]*S/,"",$6); print}' > $INPUT_BAM.noS.sam
