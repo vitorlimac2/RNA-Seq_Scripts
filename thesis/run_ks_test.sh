59ae21cae39390b91d09b3e12d3d2e358aa2c5da
Vitor Coelho <vitor@lennon.localdomain>
2018-08-29 16:24:26 -0300

    update

---
+++ thesis/run_ks_test.sh
@@ -0,0 +1,22 @@
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProC1.MappedReadInfo ProC1.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProC2.MappedReadInfo ProC2.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProC3.MappedReadInfo ProC3.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProC4.MappedReadInfo ProC4.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProC5.MappedReadInfo ProC5.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProC6.MappedReadInfo ProC6.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProC7.MappedReadInfo ProC7.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProC8.MappedReadInfo ProC8.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProC9.MappedReadInfo ProC9.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProR1.MappedReadInfo ProR1.GeneDistFrag 20
+
+Rscript --vanilla ~/git/RNA-Seq_Scripts/thesis/ksDistFrag.R ProR2.MappedReadInfo ProR2.GeneDistFrag 20
+
