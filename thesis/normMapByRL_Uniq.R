3ebb56534e0c46d5e8e660acc2d8cb9e781d6c06
Vitor Coelho <vitor@lennon.localdomain>
2018-11-18 22:02:01 -0200

    Fix bugs for empty 3th column

--- thesis/normMapByRL_Uniq.R
+++ thesis/normMapByRL_Uniq.R
@@ -13,7 +13,10 @@ inputFile <- args[1]
 
 f1 <- read.table(inputFile, sep="\t")
 
-length_bins <- pretty(f1[f1$V4 == 1,]$V2, n=5)
+nrow(f1)
+length_bins <- pretty(f1$V2, n=5)
+
+#length_bins <- seq(min(f1$V2),max(f1$V2),by = 20)
 
 length_bins[1] <- min(f1$V2)
 length_bins[length(length_bins)] <- max(f1$V2);
@@ -41,8 +44,8 @@ for(i in seq_len(length(length_bins)-1)){
 
 ## overall mapping rate
 mapping_rate_vector <- mapped_read_vector*100/(mapped_read_vector+unmapped_read_vector);
-
-x <- mapping_rate_vector[mapping_rate_vector!=Inf & mapping_rate_vector!=0]
+mapping_rate_vector
+x <- mapping_rate_vector[mapping_rate_vector!=Inf]
 
 ## Calculate median mapping rate
 m <- median(x,na.rm = T);
@@ -51,29 +54,33 @@ m
 
 ## Calculate normalization factor for each bin
 normalization_factor <- m/mapping_rate_vector;
-mapping_rate_vector
 normalization_factor
 ## Assign the normalization factor to each read
 
-temp_file <- f1[f1$V4!=0,]
-
-rm("f1")
-
-temp_file$V5 <- 0
+f1$V5 <- 0
 
 for(i in seq_len(length(length_bins)-1)){
   x <- i;
   y <- i+1;
   
   if(y != length(length_bins)){
-    temp_file[temp_file$V2 >= length_bins[x] & temp_file$V2 < length_bins[y],]$V5 <- normalization_factor[y];
+    if(normalization_factor[y]=="Inf" | normalization_factor[y]=="NaN"){
+      f1[f1$V2 >= length_bins[x] & f1$V2 < length_bins[y],]$V5 <- 0;
+    }else{
+        f1[f1$V2 >= length_bins[x] & f1$V2 < length_bins[y],]$V5 <- normalization_factor[y];
+    }
+    
+  }else if(normalization_factor[y]=="Inf" | normalization_factor[y]=="NaN"){
+    f1[f1$V2 >= length_bins[x] & f1$V2 <= length_bins[y],]$V5 <- 0;
   }else{
-    temp_file[temp_file$V2 >= length_bins[x] & temp_file$V2 <= length_bins[y],]$V5 <- normalization_factor[y];
+    f1[f1$V2 >= length_bins[x] & f1$V2 <= length_bins[y],]$V5 <- normalization_factor[y];
+    
   }
 }
 
+summary(f1$V5)
+
 ## Print the new gene count with normalization factor
 
-write.table(temp_file,paste(inputFile,"ReadCountNormalizedByRL_Unique",sep = "."),sep = "\t", quote = F, row.names = F, col.names = F);
+write.table(f1,paste(inputFile,"ReadCountNormalizedByRL_Unique",sep = "."),sep = "\t", quote = F, row.names = F, col.names = F);
 
-rm("temp_file")
