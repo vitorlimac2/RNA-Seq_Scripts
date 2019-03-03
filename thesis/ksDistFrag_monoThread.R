cc9f6b8150b5762a579a5fc5a5799400d3639068
vitor <vitor@ibm-server.biof>
2019-03-03 15:26:25 -0300

    update

--- thesis/ksDistFrag_monoThread.R
+++ thesis/ksDistFrag_monoThread.R
@@ -1,13 +1,13 @@
 #!/usr/bin/env Rscript
-
+args <- commandArgs(trailingOnly=TRUE)
 ################################################################
 ###### FUNCTIONS
 
 ks.test.from.list <- function(geneid){
   
-  dist_frag_gene <- strsplit(as.character(replicate[geneid,1]),",")
+  dist_frag_gene <- strsplit(as.character(levels(replicate[geneid,1]))[replicate[geneid,1]],",")
   dist_frag_gene <- as.numeric(unlist(dist_frag_gene))
-  
+
   d1 <- ks.test(dist_frag_gene,dist_frag_replicate, alternative="two.sided")
   d2 <- ks.test(dist_frag_gene,dist_frag_replicate, alternative="less")
   d3 <- ks.test(dist_frag_gene,dist_frag_replicate, alternative="greater")
@@ -15,7 +15,9 @@ ks.test.from.list <- function(geneid){
   out_two.sided <- paste(d1$statistic,d1$p.value,sep = ";")
   out_less <- paste(d2$statistic,d2$p.value,sep = ";")
   out_greater <- paste(d3$statistic,d3$p.value,sep = ";")
-  output_line <- paste(gene, out_less, out_two.sided, out_greater, sep = " ")
+  output_line <- paste(geneid, out_less, out_two.sided, out_greater, sep = " ")
+  
+  return(output_line)
 
 }
 
@@ -24,17 +26,17 @@ ks.test.from.list <- function(geneid){
 # try it: read.table(gzfile("/tmp/foo.csv.gz"))
 
 ## list of genes and their mapped read lengths
-replicate<- read.table(args[1], header=F);
+replicate<- read.table(args[1], header=F, row.names=1);
+#replicate<- read.table("/media/vitor/Seagate Expansion Drive/Thesis/DEG_ILB/ILB_9577.GeneDistFrag", header=F, row.names = 1);
+
 ## list of genes
 genes <- read.table(args[2], header = F)$V1
-  
-dist_frag_replicate <- strsplit(as.character(replicate[,1]),",")
+
+dist_frag_replicate <- strsplit(as.character(levels(replicate[,1])[replicate[,1]]),",")
 dist_frag_replicate <- as.numeric(unlist(dist_frag_replicate))
+
+genes <- as.character(levels(genes))[genes]
   
 for(i in 1:length(genes)){
-  ks.test.from.list(as.character(genes[i]))
+  print(ks.test.from.list(genes[i]), quote=F)
 }
-
-
-
-
